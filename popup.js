3d2d1f9027808216b12e6471abde6378f8e345a9
tchenu <tchenu@tchenus-imac.home>
2019-12-28 14:01:48 +0100

    Change icon and add security content file in popup

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
+/**
+ * Send message to background to inquire the security state
+ * @see https://developer.chrome.com/extensions/runtime#method-sendMessage
+ */
+chrome.runtime.sendMessage({popup: true});
+
+/**
+ * Intercept response from background to get the content of the security file and insert it
+ * @see https://developer.chrome.com/extensions/runtime#event-onMessage
+ */
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.tab !== undefined ) {
@@ -5,4 +15,3 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
-chrome.runtime.sendMessage({popup: true});
